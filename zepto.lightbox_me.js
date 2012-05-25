fbb855d0ad3bf5f4a18822b905f97ca2b0cf29c2
Dan Wellman <danwellman@hotmail.com>
2012-05-25 09:01:27 +0100

    Zepto port of Lightbox_me

---
+++ zepto.lightbox_me.js
@@ -0,0 +1,175 @@
+/*
+* $ lightbox_me
+* By: Buck Wilson
+* Ported to Zepto by :Dan Wellman
+* Version : 2.3z
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+(function ($) {
+
+    $.fn.lightbox_me = function (options) {
+
+        return this.each(function () {
+
+            var opts = $.extend({}, $.fn.lightbox_me.defaults, options),
+                $overlay = $(),
+                $self = $(this);
+
+            if (opts.showOverlay) {
+                //check if there's an existing overlay, if so, make subequent ones clear
+                var $currentOverlays = $(".js_lb_overlay");
+                if ($currentOverlays.length > 0) {
+                    $overlay = $('<div class="lb_overlay_clear js_lb_overlay"/>');
+                } else {
+                    $overlay = $('<div class="' + opts.classPrefix + '_overlay js_lb_overlay"/>');
+                }
+            }
+
+            // DOM Building
+            $('body').append($self.hide()).append($overlay);
+
+            // Overlay CSS stuffs
+
+            // set css of the overlay
+            if (opts.showOverlay) {
+                setOverlayHeight();
+                $overlay.css({ position: 'absolute', width: '100%', top: 0, left: 0, right: 0, bottom: 0, zIndex: (opts.zIndex + 2), display: 'none' });
+                if (!$overlay.hasClass('lb_overlay_clear')) {
+                    $overlay.css(opts.overlayCSS);
+                }
+            }
+
+            // Show it
+            if (opts.showOverlay) {
+                $overlay.show();
+                $self.show();
+                setSelfPosition();
+            } else {
+                $self.show();
+                setSelfPosition();
+            }
+
+            // Hide parent if parent specified (parentLightbox should be jquery reference to any parent lightbox)
+            if (opts.parentLightbox) {
+                opts.parentLightbox.hide();
+            }
+
+            // Bind Events
+            $(window).resize(setOverlayHeight)
+                     .resize(setSelfPosition)
+                     .scroll(setSelfPosition);
+
+            $(window).bind('keyup.lightbox_me', observeKeyPress);
+
+            if (opts.closeClick) {
+                $overlay.click(function (e) { closeLightbox(); e.preventDefault; });
+            }
+            $self.delegate(opts.closeSelector, "click", function (e) {
+                closeLightbox(); e.preventDefault();
+            });
+            $self.bind('close', closeLightbox);
+            $self.bind('reposition', setSelfPosition);
+
+            // Private Functions
+
+            // Remove or hide all elements
+            function closeLightbox() {
+                var s = $self[0].style;
+                if (opts.destroyOnClose) {
+                    $self.add($overlay).remove();
+                } else {
+                    $self.add($overlay).hide();
+                }
+
+                //show the hidden parent lightbox
+                if (opts.parentLightbox) {
+                    opts.parentLightbox.fadeIn(200);
+                }
+
+                // clean up events.
+                $self.undelegate(opts.closeSelector, "click");
+
+                $(window).unbind('reposition', setOverlayHeight);
+                $(window).unbind('reposition', setSelfPosition);
+                $(window).unbind('scroll', setSelfPosition);
+                $(window).unbind('keyup.lightbox_me');
+                opts.onClose();
+            }
+
+
+            // Function to bind to the window to observe the escape/enter key press
+            function observeKeyPress(e) {
+                if ((e.keyCode == 27 || (e.DOM_VK_ESCAPE == 27 && e.which == 0)) && opts.closeEsc) closeLightbox();
+            }
+
+
+            // Set the height of the overlay (if the document height is taller than the window, then set the overlay height to the document height, otherwise just set overlay height: 100%
+            function setOverlayHeight() {
+                if ($(window).height() < $(document).height()) {
+                    $overlay.css({ height: $(document).height() + 'px' });
+                } else {
+                    $overlay.css({ height: '100%' });
+                }
+            }
+
+            // Set the position of the modal'd window (if $self is taller than the window, then make it absolutely positioned, otherwise fixed
+            function setSelfPosition() {
+                var s = $self[0].style;
+
+                // reset CSS so width is re-calculated for margin-left CSS
+                $self.css({ left: '50%', marginLeft: (Math.floor($self.width() / 2 * -1)), zIndex: (opts.zIndex + 3) });
+
+                // if the height of $self is bigger than the window and self isn't already position absolute
+                if (($self.height() + 80 >= $(window).height()) && ($self.css('position') != 'absolute')) {
+
+                    // we are going to make it positioned where the user can see it, but they can still scroll so the top offset is based on the user's scroll position.
+                    var topOffset = $(document).scrollTop() + 40;
+                    $self.css({ position: 'absolute', top: topOffset + 'px', marginTop: 0 })
+                } else if ($self.height() + 80 < $(window).height()) {
+                    //if the height is less than the window height, then we're gonna make this thing position: fixed.
+                    if (opts.centered) {
+                        $self.css({ position: 'fixed', top: '50%', marginTop: ($self.height() / 2) * -1 })
+                    } else {
+                        $self.css({ position: 'fixed' }).css(opts.modalCSS);
+                    }
+                }
+            }
+        });
+    };
+
+    $.fn.lightbox_me.defaults = {
+
+        // close
+        closeSelector: ".close",
+        closeClick: true,
+        closeEsc: true,
+
+        // behavior
+        destroyOnClose: false,
+        showOverlay: true,
+        parentLightbox: false,
+
+        // callbacks
+        onLoad: function () { },
+        onClose: function () { },
+
+        // style
+        classPrefix: 'lb',
+        zIndex: 999,
+        centered: false,
+        modalCSS: { top: '40px' },
+        overlayCSS: { background: 'black', opacity: .3 }
+    }
+}(Zepto));
